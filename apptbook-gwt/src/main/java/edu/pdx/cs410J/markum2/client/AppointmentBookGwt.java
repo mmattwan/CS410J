3c31ffedbe6194dbbe876e23d22b00ba2635872d
mmattwan <mmattwandel@frontier.com>
2016-07-29 18:10:12 -0700

    After Lecture part 1.

---
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/markum2/client/AppointmentBookGwt.java
@@ -0,0 +1,98 @@
+package edu.pdx.cs410J.markum2.client;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import edu.pdx.cs410J.AbstractAppointment;
+
+import java.util.Collection;
+
+/**
+ * A basic GWT class that makes sure that we can send an appointment book back from the server
+ */
+public class  AppointmentBookGwt implements EntryPoint {
+  private final Alerter alerter;
+
+  @VisibleForTesting
+  Button button;
+  TextBox textBox;
+
+  public AppointmentBookGwt() {
+    this(new Alerter() {
+      @Override
+      public void alert(String message) {
+        Window.alert(message);
+      }
+    });
+  }
+
+  @VisibleForTesting
+  AppointmentBookGwt(Alerter alerter) {
+    this.alerter = alerter;
+
+    addWidgets();
+  }
+
+  private void addWidgets() {
+    this.textBox = new TextBox();
+    button = new Button("Ping Server");
+    pingServer();
+  }
+
+  private void pingServer() {
+    button.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent clickEvent) {
+        PingServiceAsync async = GWT.create(PingService.class);
+        async.ping(new AsyncCallback<AppointmentBook>() {
+
+          @Override
+          public void onSuccess(AppointmentBook airline) {
+            displayInAlertDialog(airline);
+          }
+
+          @Override
+          public void onFailure(Throwable ex) {
+             alert(ex);
+          }
+        });
+      }
+    });
+  }
+
+  private void displayInAlertDialog(AppointmentBook airline) {
+    StringBuilder sb = new StringBuilder(airline.toString());
+    sb.append("\n");
+
+    Collection<Appointment> flights = airline.getAppointments();
+    for (Appointment flight : flights) {
+      sb.append(flight);
+      sb.append("\n");
+    }
+    alerter.alert(sb.toString());
+  }
+
+  private void alert(Throwable ex) {
+    alerter.alert(ex.toString());
+  }
+
+  @Override
+  public void onModuleLoad() {
+    RootPanel rootPanel = RootPanel.get();
+    rootPanel.add(button);
+    rootPanel.add(textBox);
+  }
+
+  @VisibleForTesting
+  interface Alerter {
+    void alert(String message);
+  }
+
+}
