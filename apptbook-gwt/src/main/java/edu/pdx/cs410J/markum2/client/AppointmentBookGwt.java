e96de01be21c28d2a00d345ee6737cdd1f5075ed
mmattwan <mmattwandel@frontier.com>
2016-08-02 16:10:40 -0700

    Before adding my Java Classes.

--- apptbook-gwt/src/main/java/edu/pdx/cs410J/markum2/client/AppointmentBookGwt.java
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/markum2/client/AppointmentBookGwt.java
@@ -21,11 +21,11 @@ public class AppointmentBookGwt implements EntryPoint {
   private Button createButton;
   private Button exportButton;
   private Button importButton;
-  private Button showButton;
   private Button helpButton;
   private TextBox ownerTextBox, startTextBox, endTextBox, descTextBox;
   private TextBox exportTextBox, importTextBox;
   private TextBox textBox;
+  private TextArea appointmentBookTextArea;
 
   public AppointmentBookGwt() {
     this(new Alerter() {
@@ -47,34 +47,44 @@ public class AppointmentBookGwt implements EntryPoint {
     createButton.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent clickEvent) {
+
+        String s="";
+        s += ownerTextBox.getText()+"\n";
+        s += descTextBox.getText()+"\n";
+        s += startTextBox.getText()+"\n";
+        s += endTextBox.getText()+"\n";
+        appointmentBookTextArea.setText(s);
+
+        Window.alert("create clicked");
         createAppointments();
       }
     });
 
     helpButton = new Button("Help");
-/*
     helpButton.addClickHandler(new ClickHandler() {
       @Override
-      public void onHelpClick(ClickEvent helpClickEvent) {
-        showHelp();
+      public void onClick(ClickEvent clickEvent) {
+        Window.alert("help clicked");
+//        showHelp();
       }
     });
-*/
+
     this.ownerTextBox = new TextBox();
     this.descTextBox = new TextBox();
     this.startTextBox = new TextBox();
     this.endTextBox = new TextBox();
     this.exportTextBox = new TextBox();
     this.importTextBox = new TextBox();
+    this.appointmentBookTextArea = new TextArea();
 
     exportButton = new Button("Write to file");
     importButton = new Button("Read from file");
-    showButton = new Button("Show Appointments");
     helpButton = new Button("Help");
 
     this.textBox = new TextBox();
    }
 
+  // begin createButton
   // createButton  createButton  createButton  createButton  createButton  createButton
   private void createAppointments() {
     PingServiceAsync async = GWT.create(PingService.class);
@@ -111,46 +121,31 @@ public class AppointmentBookGwt implements EntryPoint {
     }
     alerter.alert(sb.toString());
   }
-/*
+
+  // end createButton
+
   // start showHelp
-  // showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp
-  private void createAppointments() {
-    PingServiceAsync async = GWT.create(PingService.class);
-    int numberOfAppointments = getNumberOfAppointments();
-    async.createAppointmentBook(numberOfAppointments, new AsyncCallback<AppointmentBook>() {
+  private void showHelp() {
+
+  //    HelpServiceAsync async = GWT.create(HelpService.class);
+/*
+//    asyncb.createAppointmentBook(numberOfAppointments, new AsyncCallback<AppointmentBook>() {
+    async.showHelp( {
 
       @Override
-      public void onSuccess(AppointmentBook airline) {
-        displayInAlertDialog(airline);
+      public void onSuccess() {
+        alerter.alert("Help");
       }
 
       @Override
       public void onFailure(Throwable ex) {
         alert(ex);
       }
+
     });
-  }
-
-  // showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp
-  private int getNumberOfAppointments() {
-    String number = this.textBox.getText();
-    return Integer.parseInt(number);
-  }
-
-  // showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp  showHelp
-  private void displayInAlertDialog(AppointmentBook airline) {
-    StringBuilder sb = new StringBuilder(airline.toString());
-    sb.append("\n");
-
-    Collection<Appointment> flights = airline.getAppointments();
-    for (Appointment flight : flights) {
-      sb.append(flight);
-      sb.append("\n");
-    }
-    alerter.alert(sb.toString());
+*/
   }
 // end showHelp
-*/
 
 
   private void alert(Throwable ex) {
@@ -166,20 +161,32 @@ public class AppointmentBookGwt implements EntryPoint {
   public void onModuleLoad() {
     RootPanel rootPanel = RootPanel.get();
 
-    VerticalPanel verticalPanel1 = new VerticalPanel();  VerticalPanel verticalPanel2 = new VerticalPanel();
-    verticalPanel1.setSpacing(20);                       verticalPanel2.setSpacing(17);
-    verticalPanel1.add(new Label("Owner"));              verticalPanel2.add(ownerTextBox);
-    verticalPanel1.add(new Label("Description"));        verticalPanel2.add(descTextBox);
-    verticalPanel1.add(new Label("Start"));              verticalPanel2.add(startTextBox);
-    verticalPanel1.add(new Label("End"));                verticalPanel2.add(endTextBox);
-    verticalPanel1.add(createButton);                    verticalPanel2.add(new Label("."));
-    verticalPanel1.add(exportButton);                    verticalPanel2.add(exportTextBox);
-    verticalPanel1.add(importButton);                    verticalPanel2.add(importTextBox);
-    verticalPanel1.add(showButton);
-    verticalPanel1.add(helpButton);
+    appointmentBookTextArea.setHeight("400");
+    appointmentBookTextArea.setVisibleLines(40);
+    appointmentBookTextArea.setCharacterWidth(100);
+    appointmentBookTextArea.setText("Hello");
+    appointmentBookTextArea.setReadOnly(true);
+
+    ownerTextBox.setVisibleLength(30);
+    startTextBox.setVisibleLength(19);
+    endTextBox.setVisibleLength(19);
+    descTextBox.setVisibleLength(30);
+    exportTextBox.setVisibleLength(30);
+    importTextBox.setVisibleLength(30);
+
+    VerticalPanel vertPanel1 = new VerticalPanel(), vertPanel2 = new VerticalPanel(), vertPanel3 = new VerticalPanel();
+    vertPanel1.setSpacing(20);                      vertPanel2.setSpacing(17);        vertPanel3.add(appointmentBookTextArea);
+    vertPanel1.add(new Label("Owner"));             vertPanel2.add(ownerTextBox);
+    vertPanel1.add(new Label("Description"));       vertPanel2.add(descTextBox);
+    vertPanel1.add(new Label("Start"));             vertPanel2.add(startTextBox);
+    vertPanel1.add(new Label("End"));               vertPanel2.add(endTextBox);
+    vertPanel1.add(createButton);                   vertPanel2.add(new Label("."));
+    vertPanel1.add(exportButton);                   vertPanel2.add(exportTextBox);
+    vertPanel1.add(importButton);                   vertPanel2.add(importTextBox);
+    vertPanel1.add(helpButton);
 
     HorizontalPanel horizontalPanel = new HorizontalPanel();
-    horizontalPanel.add(verticalPanel1);                 horizontalPanel.add(verticalPanel2);
+    horizontalPanel.add(vertPanel1);           horizontalPanel.add(vertPanel2);  horizontalPanel.add(vertPanel3);
     rootPanel.add(horizontalPanel);
     rootPanel.add(textBox);
   }
